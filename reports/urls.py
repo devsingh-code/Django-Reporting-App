dc043d9b4ebc26208229dbb80fab09f733996949
Deovrat Singh <devsingh6n@gmail.com>
2020-08-23 16:29:07 +0530

    Added Report logic in views and report page styling

--- reports/urls.py
+++ reports/urls.py
@@ -4,6 +4,6 @@ from .views import report_view
 app_name ='reports'
 
 urlpatterns = [
-    path('', report_view, name='report-view'),
+    path('<str:production_line>/', report_view, name='report-view'),
 ]
 
